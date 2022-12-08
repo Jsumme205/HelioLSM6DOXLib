b7e17a9ab5062e00f7d3aac1608d7b8661156638
Jsumme205 <119705812+Jsumme205@users.noreply.github.com>
2022-12-07 18:03:14 -0800

    Update readAccelGyro.ino

--- readAccelGyro.ino
+++ readAccelGyro.ino
@@ -11,10 +11,25 @@ ProtoLSM6DSOXLib IMU(IMUADD);
 void setup(){
 
 serial.begin(9600);
-
+MyInitializer();
   
 }
 
 void loop() {
 
+}
+
+void MyInitializer(){
+  serial.begin(9600);
+  serial.printIn("initializing ProtoLSM6DSOXLib library, this will take a second");
+  delay(500);
+  serial.printIn("initializing...");
+  delay(500); 
+  serial.printIn("initializing...");
+  delay(500);
+  serial.printIn("initializing...");
+  delay(500);
+  serial.printIn("initialization complete");
+  delay(250);
+  serial.printIn("Welcome to ProtoLSM6DSOXLib");
 }
