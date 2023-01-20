c0e2b8a88f8b9baa5b5910925379e41db2059ac4
HelioLabs <119705812+Jsumme205@users.noreply.github.com>
2023-01-19 21:40:31 -0800

    Update HelioLSM6DSOXLib.h

--- src/HelioLSM6DSOXLib.h
+++ src/HelioLSM6DSOXLib.h
@@ -5,10 +5,10 @@
 // libraries that are used in this other library  
 
 // this is the main arduino library. It defines all of the pins, as well as all the functions that are unique to the microcontroller
-// #include <Arduino.h>
+ #include <Arduino.h>
 
-// this enables the I2C interface. It brings the Wire. functions into play
- // #include <Wire.h>
+// this enables the I2C interface. It brings the Wire functions into play
+  #include <Wire.h>
 
 // creates a class, where all of the LSM6DSOX functions are contained in. Use this for easy reference to all of the functions.
 class ProtoLSM6DSOXLib {
@@ -33,18 +33,16 @@ class ProtoLSM6DSOXLib {
     void readTempF();
     void readTempC();
     
-    // these functions read the First in, First out byte buffer, which means the first byte to get there gets processed first and the newest byte gets processed last 
+    // this function reads the First in, First out byte buffer, which means the first byte to get there gets processed first and the newest byte gets processed last 
     void GetFIFOStat();
-    void GetFIFOStat2(int thresh);
     
-    // these both are time functions the first is the time, the second is if you want to 
+    // these both are time functions the first is the time, the second is if you want to set it to your timezone (-12 to 12) 
     void getTime();
     void setTimeZone(int TimeZone);
     
     // these are private variables that can only be used or modified in the class function (this function)
     private:
     int _address;
-    int _thresh;
     int _TimeZone;
 };
 
